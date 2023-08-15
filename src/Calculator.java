d6ee35011bbc4d0087428476829d9d01105eea7b
sserh <serhios@bk.ru>
2023-08-15 09:31:57 +0700

    aren't working with exceptions now

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,16 +8,15 @@ public class Calculator {
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
     BinaryOperator<Integer> divide = (x, y) -> {
-        //обернём в try-catch, чтобы программа завершалась корректно.
+        //учтём, что в делитель теоретически можно передать 0 и обработаем эту ситуацию
         //возврат 0 в случае деления на 0, конечно, тоже не очень хорошо, так как это вполне себе значение,
         //но чтобы этого не делать (возвращать 0), нужно править код в Mаin, а этого, наверное, нельзя делать
-        int result = 0;
-        try {
-            result = x / y;
-        } catch (ArithmeticException e) {
-            System.out.println("Обнаружено деление на 0, в консоль будет будет выведен 0, но это невалидное значение");
+        if (y == 0) {
+            System.out.println("Обнаружено деление на 0, в консоль будет будет выведено максимальное значение Integer, но это невалидное значение");
+            return Integer.MAX_VALUE;
+        } else {
+            return x / y;
         }
-        return result;
     };
 
     UnaryOperator<Integer> pow = x -> x * x;
